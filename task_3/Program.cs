7f2708528d839a149f788085fd68d55b35a038a9
SvyatoslavLesnikov <svyatoslav.newpolzovatel@gmail.com>
2024-08-13 22:11:22 +0300

    Решение 3 задачи

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца.
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+void PrintArrayReversed(int[] array, int n)
+{
+    if (n > 0)
+    {
+        Console.WriteLine(n + " ");
+        PrintArrayReversed(array, n - 1);
+    }
+}
+PrintArrayReversed(array, array.Length - 1);
