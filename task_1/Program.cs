25c487b453925ebe743201c786e2726f75588f1d
SvyatoslavLesnikov <svyatoslav.newpolzovatel@gmail.com>
2024-08-13 22:08:20 +0300

    Решение первой задачи

---
+++ task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+void PrintNaturalNumbersInRange(int M, int N)
+{
+    if (M <= N)
+    {
+        Console.WriteLine(M);
+        PrintNaturalNumbersInRange(M + 1, N);
+    }
+}
+PrintNaturalNumbersInRange(1,6);
